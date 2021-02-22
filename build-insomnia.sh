fd8e1cd4d2bfe91dab394816e9a496d231f0fb35
Keanu Kerr <keanukerr@gmail.com>
2021-02-22 01:36:38 -0600

    Script now creates executables for Linux, macOS, and Windows

--- build-insomnia.sh
+++ build-insomnia.sh
@@ -17,7 +17,29 @@ sed -i 's/updateAutomatically: true/updateAutomatically: false/' \
 sed -i 's/disableUpdateNotification: false/disableUpdateNotification: true/' \
     $EXTRACTED/packages/insomnia-app/app/models/settings.js
 
-# cat $EXTRACTED/packages/insomnia-app/app/models/settings.js
+# Set up node and dependencies
+npm install -g electron
+npm install -g electron-builder
+npm explore npm -g -- npm install node-gyp@latest
 
-# Package Insomnia
-electron-packager $EXTRACTED
+# Install dependencies
+npm run bootstrap --prefix $EXTRACTED
+export GIT_TAG=$VERSION
+
+# Build for Linux
+sed -i 's/targetPlatform = PLATFORM_MAP.*/targetPlatform = PLATFORM_MAP.linux;/' \
+    $EXTRACTED/packages/insomnia-app/scripts/package.js
+npm run app-package --prefix $EXTRACTED
+cp insomnia-core-2020.5.2/packages/insomnia-app/dist/com.insomnia.app/*.AppImage .
+
+# Build for macOS
+sed -i 's/targetPlatform = PLATFORM_MAP.*/targetPlatform = PLATFORM_MAP.darwin;/' \
+    $EXTRACTED/packages/insomnia-app/scripts/package.js
+npm run app-package --prefix $EXTRACTED
+cp -R insomnia-core-2020.5.2/packages/insomnia-app/dist/com.insomnia.app/mac/*.app .
+
+# Build for Windows
+sed -i 's/targetPlatform = PLATFORM_MAP.*/targetPlatform = PLATFORM_MAP.win32;/' \
+    $EXTRACTED/packages/insomnia-app/scripts/package.js
+npm run app-package --prefix $EXTRACTED
+cp insomnia-core-2020.5.2/packages/insomnia-app/dist/com.insomnia.app/squirrel-windows/*.exe .
