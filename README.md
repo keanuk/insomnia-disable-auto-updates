f686ff7fef418a10df7df28d2c54a84292f1e36f
Keanu Kerr <keanukerr@gmail.com>
2021-02-22 09:50:55 -0600

    Updated README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # insomnia-disable-auto-updates
 Script which modifies and builds Insomnia Core with auto updates disabled
+
+## Usage
+Execute 'run.sh' with no parameters in order to pull the latest version of Insomnia and modify the source code to disable auto updating.
+
+Execute 'run.sh build' to attempt to build Insomnia for all available platforms (Linux, macOS, and Windows).
+
+Execute 'run.sh build $PLATFORM' to attempt to build Insomnia for specified platform.
+Available platforms:
+- 'linux' (Linux)
+- 'darwin' (macOS)
+- 'win32' (Windows) 
--- build-insomnia.sh
+++ build-insomnia.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
-# Download latest source tar from GitHub
 VERSION=$(curl --silent "https://api.github.com/repos/Kong/insomnia/releases/latest" |
     grep -oP '"tag_name": "\K(.*)(?=")')
-echo "Latest version of Insomnia Core is" $VERSION
-curl -sOL "https://github.com/Kong/insomnia/archive/"$VERSION'.tar.gz'
-
-# Extract tar
-tar -xzf $VERSION'.tar.gz'
 EXTRACTED=$(tar -tzf $VERSION'.tar.gz' | head -1 | cut -f1 -d"/")
-echo "Extracted latest version to" $EXTRACTED
-
-# Modify auto update settings
-sed -i 's/updateAutomatically: true/updateAutomatically: false/' \
-	$EXTRACTED/packages/insomnia-app/app/models/settings.js
-sed -i 's/disableUpdateNotification: false/disableUpdateNotification: true/' \
-    $EXTRACTED/packages/insomnia-app/app/models/settings.js
 
 # Set up node and dependencies
 npm install -g electron
@@ -26,20 +13,37 @@ npm explore npm -g -- npm install node-gyp@latest
 npm run bootstrap --prefix $EXTRACTED
 export GIT_TAG=$VERSION
 
-# Build for Linux
-sed -i 's/targetPlatform = PLATFORM_MAP.*/targetPlatform = PLATFORM_MAP.linux;/' \
-    $EXTRACTED/packages/insomnia-app/scripts/package.js
-npm run app-package --prefix $EXTRACTED
-cp insomnia-core-2020.5.2/packages/insomnia-app/dist/com.insomnia.app/*.AppImage .
+# Check if platform was specified in command line
+if [ -z "$1" ]
+    then
+        echo "Building Insomnia for linux, darwin, and win32"
 
-# Build for macOS
-sed -i 's/targetPlatform = PLATFORM_MAP.*/targetPlatform = PLATFORM_MAP.darwin;/' \
-    $EXTRACTED/packages/insomnia-app/scripts/package.js
-npm run app-package --prefix $EXTRACTED
-cp -R insomnia-core-2020.5.2/packages/insomnia-app/dist/com.insomnia.app/mac/*.app .
+        # Build for Linux
+        sed -i 's/targetPlatform = PLATFORM_MAP.*/targetPlatform = PLATFORM_MAP.linux;/' \
+            $EXTRACTED/packages/insomnia-app/scripts/package.js
+        npm run app-package --prefix $EXTRACTED
+        cp insomnia-core-2020.5.2/packages/insomnia-app/dist/com.insomnia.app/*.AppImage .
 
-# Build for Windows
-sed -i 's/targetPlatform = PLATFORM_MAP.*/targetPlatform = PLATFORM_MAP.win32;/' \
-    $EXTRACTED/packages/insomnia-app/scripts/package.js
-npm run app-package --prefix $EXTRACTED
-cp insomnia-core-2020.5.2/packages/insomnia-app/dist/com.insomnia.app/squirrel-windows/*.exe .
+        # Build for macOS
+        sed -i 's/targetPlatform = PLATFORM_MAP.*/targetPlatform = PLATFORM_MAP.darwin;/' \
+            $EXTRACTED/packages/insomnia-app/scripts/package.js
+        npm run app-package --prefix $EXTRACTED
+        cp -R insomnia-core-2020.5.2/packages/insomnia-app/dist/com.insomnia.app/mac/*.app .
+
+        # Build for Windows
+        sed -i 's/targetPlatform = PLATFORM_MAP.*/targetPlatform = PLATFORM_MAP.win32;/' \
+            $EXTRACTED/packages/insomnia-app/scripts/package.js
+        npm run app-package --prefix $EXTRACTED
+        cp insomnia-core-2020.5.2/packages/insomnia-app/dist/com.insomnia.app/squirrel-windows/*.exe .
+
+    else
+        echo "Building Insomnia for" $1
+
+        # Build for specified platform
+        sed -i 's/targetPlatform = PLATFORM_MAP.*/targetPlatform = PLATFORM_MAP.'"$1"';/' \
+            $EXTRACTED/packages/insomnia-app/scripts/package.js
+        npm run app-package --prefix $EXTRACTED
+        cp insomnia-core-2020.5.2/packages/insomnia-app/dist/com.insomnia.app/*.AppImage .
+        cp insomnia-core-2020.5.2/packages/insomnia-app/dist/com.insomnia.app/mac/*.app .
+        cp insomnia-core-2020.5.2/packages/insomnia-app/dist/com.insomnia.app/squirrel-windows/*.exe .
+fi
